ef2a0d6e49e7ddaa7aec2700fd484510ba40e26c
fulong <834424581@qq.com>
2017-07-03 19:07:00 +0800

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User # Django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是 Django 为我们已经写好的用户模型。
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse ('blog:detail', kwargs={'pk': self.pk})
---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'blog' #视图函数命名空间,防止冲突
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+]
