90a87b7069e14cf40e2c72027e3df788269447da
fulong <834424581@qq.com>
2017-07-05 15:24:20 +0800

    创建文章摘要

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User # Django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是 Django 为我们已经写好的用户模型。
+import markdown
+from django.utils.html import strip_tags
 
 class Category(models.Model):
+
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -36,6 +39,20 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views']) # 只更新views字段
+
+    # 重写模型save方法，自动保存文章正文前45个字到摘要
+    def save(self,*args,**kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(
+                extensions=[
+                    'markdown.extensions.extra',
+                    'markdown.extensions.codehilite',
+                ]
+            )
+            self.excerpt = strip_tags(md.convert(self.body)[:45])
+
+        # 调用父类save方法，讲数据保存到数据库中
+        super(Post,self).save(*args,**kwargs)
 
     class Meta:
         ordering = ['-created_time']
