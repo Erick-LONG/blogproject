824608ecd3d45d67ad14745cc23a319edaad3401
fulong <834424581@qq.com>
2017-07-10 18:44:39 +0800

    简单搜索功能

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,18 @@ from comments.forms import CommentForm
 from django.views.generic import ListView,DetailView
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
+from django.db.models import Q # 包装查询表达式
+
+def search(request):
+	q = request.GET.get('q')
+	error_msg = ''
+
+	if not q :
+		error_msg = '请输入关键词'
+		return render(request,'blog/index.html',{'error_msg':error_msg})
+
+	post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__contains=q)) # Field lookups在模型需要筛选的属性后面跟上两个下划线
+	return render(request,'blog/index.html',{'error_msg':error_msg,'post_list':post_list})
 
 class IndexView(ListView):
 	model = Post
